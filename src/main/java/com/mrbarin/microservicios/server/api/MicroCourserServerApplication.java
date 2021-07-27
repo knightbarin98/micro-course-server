e999611e5698ca12915cca06eae619950a026c20
ivansoto <lopezsotoivan01@gmail.com>
2021-07-27 16:34:07 -0500

    cambiar configuracion

--- src/main/java/com/mrbarin/microservicios/server/api/MicroCourserServerApplication.java
+++ src/main/java/com/mrbarin/microservicios/server/api/MicroCourserServerApplication.java
@@ -2,8 +2,10 @@ package com.mrbarin.microservicios.server.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class MicroCourserServerApplication {
 
 	public static void main(String[] args) {
