91f0477fa4ac6ce3a383d0be9be3788d5357dbc1
ivansoto <lopezsotoivan01@gmail.com>
2021-07-27 07:15:43 -0500

    first commit

---
+++ src/main/java/com/mrbarin/microservicios/server/api/MicroCourserServerApplication.java
@@ -0,0 +1,13 @@
+package com.mrbarin.microservicios.server.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroCourserServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroCourserServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrbarin/microservicios/server/api/MicroCourserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrbarin.microservicios.server.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroCourserServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
